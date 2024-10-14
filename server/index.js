568dd13dac0d729ae298021bfe7c0475efb97f36
nhughey <nigel.hughey@gmail.com>
2024-10-14 05:32:11 -0600

    working blueprint

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
+
 const express = require("express");
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 const { client } = require("./db");
@@ -7,6 +9,15 @@ client.connect();
 app.use(express.json());
 
 app.use("/api", require("./api"));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.use((err, req, res, next) => {
   console.log(err);
